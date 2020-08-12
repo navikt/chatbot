38f7e5e194eb0fa4f30a3127a86f9cef4cd81da7
anders <anders@nomerstad.com>
2020-08-12 14:58:17 +0200

    Fjerner z-index fra container

--- src/components/ChatContainer/styles.ts
+++ src/components/ChatContainer/styles.ts
@@ -16,7 +16,6 @@ export const Container = styled.div`
     justify-content: center;
     border: 1px solid transparent;
     box-shadow: 6px 6px 6px 0 rgba(0, 0, 0, 0);
-    z-index: 9999;
     transition: all 300ms cubic-bezier(0.86, 0, 0.07, 1);
 
     ${liten} {
@@ -46,5 +45,5 @@ export const Container = styled.div`
                 bottom: 0;
                 left: 0;
             }
-        `}
+        `};
 `;
