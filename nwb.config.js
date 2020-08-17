548c663485c094e7536c82c3b0cad96c956f41f2
anders <anders@nomerstad.com>
2020-08-17 11:45:30 +0200

    Setter styled-components tilbake til dependencies

--- nwb.config.js
+++ nwb.config.js
@@ -25,7 +25,6 @@ module.exports = {
                     'commonjs2 react-app-polyfill/stable',
                 'object-hash': 'commonjs2 object-hash',
                 moment: 'commonjs2 moment',
-                'styled-compoments': 'commonjs2 styled-compoments',
             },
         },
         rules: {
