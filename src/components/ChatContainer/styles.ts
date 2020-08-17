e663c994c5ef13e26850370fc5b54039c51bffe5
anders <anders@nomerstad.com>
2020-08-17 09:49:40 +0200

    Fjerner unødvendig element. Hindrer uønsket fokus-markering i enkelte situasjoner.

--- src/components/ChatContainer/styles.ts
+++ src/components/ChatContainer/styles.ts
@@ -20,6 +20,18 @@ export const Container = styled.div`
     border: 1px solid transparent;
     box-shadow: 6px 6px 6px 0 rgba(0, 0, 0, 0);
     transition: all 300ms cubic-bezier(0.86, 0, 0.07, 1);
+    box-sizing: border-box;
+    font-family: ${tema.tekstFamilie};
+    font-size: ${tema.storrelser.tekst.generell};
+
+    &:focus {
+        outline: none;
+    }
+
+    *:focus {
+        outline: none;
+        box-shadow: 0 0 0 3px #005b82;
+    }
 
     ${liten} {
         width: fit-content;
@@ -40,6 +52,12 @@ export const Container = styled.div`
             bottom: ${bottomOffset};
             right: ${rightOffset};
 
+            *,
+            *:before,
+            *:after {
+                box-sizing: inherit;
+            }
+            
             @media (max-height: calc(${tema.hoyde} + ${bottomOffset})) {
                 top: 0;
                 bottom: auto;
--- src/index.tsx
+++ src/index.tsx
@@ -2,31 +2,10 @@ import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 
 import React, { Component } from 'react';
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import ChatContainer from './components/ChatContainer';
 
 import tema from './tema/tema';
-
-const Global = styled.div`
-    box-sizing: border-box;
-    font-family: ${tema.tekstFamilie};
-    font-size: ${tema.storrelser.tekst.generell};
-
-    *,
-    *:before,
-    *:after {
-        box-sizing: inherit;
-    }
-
-    &:focus {
-        outline: none;
-    }
-
-    *:focus {
-        outline: none;
-        box-shadow: 0 0 0 3px #005b82;
-    }
-`;
 
 export type ConnectionConfig = {
     queueKey: string;
@@ -47,15 +26,13 @@ export default class Chat extends Component<ConnectionConfig, {}> {
         } = this.props;
         return (
             <ThemeProvider theme={tema}>
-                <Global tabIndex={-1}>
-                    <ChatContainer
-                        customerKey={customerKey}
-                        queueKey={queueKey}
-                        configId={configId}
-                        evaluationMessage={evaluationMessage}
-                        isOpen={isOpen}
-                    />
-                </Global>
+                <ChatContainer
+                    customerKey={customerKey}
+                    queueKey={queueKey}
+                    configId={configId}
+                    evaluationMessage={evaluationMessage}
+                    isOpen={isOpen}
+                />
             </ThemeProvider>
         );
     }
