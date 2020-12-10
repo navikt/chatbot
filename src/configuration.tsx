da3a7b00d7b1a5b53076696a1265ed423256bfcf
thomaslindstr_m <t@hom.as>
2020-12-10 14:41:40 +0100

    :art:

--- src/configuration.tsx
+++ src/configuration.tsx
@@ -3,7 +3,7 @@ const cookieDomain =
 
 let internalClientLanguage = 'no';
 
-if (window.location.href.indexOf('/en/') !== -1) {
+if (window.location.href.includes('/en/')) {
     internalClientLanguage = 'en';
 }
 
