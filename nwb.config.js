4af7ab41a6c6a23f1fd222f3a3a0b97ad75b4697
Terje Karlsen <terjeofnorway@gmail.com>
2022-03-29 16:16:43 +0200

    Flytter enkelte pakker til dependencies. Fjerner externals fra nwb

--- nwb.config.js
+++ nwb.config.js
@@ -7,28 +7,6 @@ module.exports = {
             externals: {
                 react: 'React',
                 '@navikt/fnrvalidator': '@navikt/fnrvalidator',
-                'nav-frontend-alertstriper': 'nav-frontend-alertstriper',
-                'nav-frontend-alertstriper-style':
-                    'nav-frontend-alertstriper-style',
-                'nav-frontend-chevron-style': 'nav-frontend-chevron-style',
-                'nav-frontend-core': 'nav-frontend-core',
-                'nav-frontend-ikoner-assets': 'nav-frontend-ikoner-assets',
-                'nav-frontend-js-utils': 'nav-frontend-js-utils',
-                'nav-frontend-knapper': 'nav-frontend-knapper',
-                'nav-frontend-knapper-style': 'nav-frontend-knapper-style',
-                'nav-frontend-lenkepanel': 'nav-frontend-lenkepanel',
-                'nav-frontend-lenkepanel-style':
-                    'nav-frontend-lenkepanel-style',
-                'nav-frontend-lenker': 'nav-frontend-lenker',
-                'nav-frontend-lenker-style': 'nav-frontend-lenker-style',
-                'nav-frontend-paneler': 'nav-frontend-paneler',
-                'nav-frontend-paneler-style': 'nav-frontend-paneler-style',
-                'nav-frontend-skjema': 'nav-frontend-skjema',
-                'nav-frontend-skjema-style': 'nav-frontend-skjema-style',
-                'nav-frontend-spinner': 'nav-frontend-spinner',
-                'nav-frontend-spinner-style': 'nav-frontend-spinner-style',
-                'nav-frontend-typografi': 'nav-frontend-typografi',
-                'nav-frontend-typografi-style': 'nav-frontend-typografi-style',
                 'styled-components': 'styled-components'
             }
         },
